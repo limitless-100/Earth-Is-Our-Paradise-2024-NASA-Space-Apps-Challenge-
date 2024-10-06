0ad9c260e3d1714a72305d978a1e43be5dcec9ca
xpanvictor <xpanvictor@gmail.com>
2024-10-06 22:52:33 +0100

    feat(dashboard): little content

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,35 @@
+import {Filter, Search} from "lucide-react";
+
+const scoreData = [
+    {
+        title: "Overall class score",
+        value: 68,
+        icon: "",
+        avg: 72
+    },
+    {
+        title: "Work Assigned",
+        value: 55,
+        icon: "",
+        avg: 64
+    }
+]
 
 function Dashboard() {
     return (
-        <div>Hello class</div>
+        <main className={"w-full h-full p-2 px-8"}>
+            <div className={"border border-gray-700 flex bg-white rounded-full w-full p-2"}>
+                <Search />
+                <input placeholder={"Search here..."} className={"flex-grow mx-2"}/>
+                <Filter />
+            </div>
+
+            <section className={"flex mt-8"}>
+                <div className={"bg-white rounded-2xl gap-4"}>
+
+                </div>
+            </section>
+        </main>
     )
 }
 
