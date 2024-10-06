7a8aeb5f8f0f7df51e4ae2fb3c3c79a19303f45e
Dubjay18 <jejeniyi7@gmail.com>
2024-10-06 14:34:17 +0100

    feat: login page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-screen bg-[#e4e0e0]">
+      <div className="hidden bg-muted lg:flex items-center justify-center bg-[#e4e0e0]">
+        <div className="flex flex-col items-center gap-4">
+          <Image
+            src="/logo2.svg"
+            alt="Image"
+            width={400}
+            height={400}
+            className=""
+          />
+          <p>This is our home, we must take care of it.</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center py-12">{children}</div>
+    </div>
+  );
+}
+
+export default layout;
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import Link from "next/link";
+function page() {
+  return (
+    <div className="mx-auto grid w-[350px] h-[80vh] px-10 rounded-xl py-4 gap-6 bg-white">
+      <div className="grid gap-2 text-center">
+        <Image
+          src="/earth.svg"
+          alt="Image"
+          className="mx-auto"
+          width={100}
+          height={100}
+        />
+        <div>
+          <h1 className="text-3xl font-bold">Welcome Back</h1>
+          <p>please enter your details</p>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <Link
+              href="/forgot-password"
+              className="ml-auto inline-block text-sm underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+      </div>
+      <div className="mt-4 text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link href="#" className="underline">
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default page;
