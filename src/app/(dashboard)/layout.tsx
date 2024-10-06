9e40f577d41592f67a4621e9060b29d10a89f71f
Dubjay18 <jejeniyi7@gmail.com>
2024-10-06 23:38:53 +0100

    chore

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,16 +1,18 @@
-"use client"
+"use client";
 import React from "react";
 import Sidebar from "@/components/sidebar";
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full lg:flex lg:min-h-[600px] p-2 lg:grid-cols-3 xl:min-h-screen bg-[#e4e0e0]">
+    <div className="w-full lg:flex  p-2 lg:grid-cols-3 min-h-screen bg-[#e4e0e0]">
       <div className="hidden lg:w-1/5 lg:flex items-center justify-center">
-          <Sidebar />
+        <Sidebar />
       </div>
-      <div className="flex-grow flex items-center justify-center py-12">{children}</div>
-       <div className="hidden lg:w-1/5 lg:flex items-center justify-center">
-          <Sidebar />
+      <div className="flex-grow flex items-center justify-center py-12">
+        {children}
+      </div>
+      <div className="hidden lg:w-1/5 lg:flex items-center justify-center">
+        <Sidebar />
       </div>
     </div>
   );
